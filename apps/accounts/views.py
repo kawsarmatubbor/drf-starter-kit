c29a1af69c22f6e3cf498dfc85f11a3ef8ffe040
kawsarmatubbor <kawsarmatubbordec@example.com>
2026-02-08 16:18:28 +0600

    add auth serializer, logic and routes

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import RegisterSerializer
 
-# Create your views here.
+@api_view(["POST"])
+def register_view(request):
+    serializer = RegisterSerializer(data = request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors)
